dcc399c225dc9b505e2c56bf35aa000ca919785b
Jumi <feliciastrandberg@gmail.com>
2023-12-11 16:01:07 +0100

    added simple fetch to get products from api

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // Get the products from the API
+async function getProducts() {
+    const response = await fetch(' https://api.escuelajs.co/api/v1/products');
+    const products = await response.json();
+    console.table(products);
+}
+
